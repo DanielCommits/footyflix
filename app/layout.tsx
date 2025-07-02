4eb3fe65302f994fff096816df04bc2fe73ee373
DanielCommits <173450189+DanielCommits@users.noreply.github.com>
2025-07-02 21:42:28 +0100

    disabled zoom on site

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,27 @@
-import type React from "react"
-import type { Metadata } from "next"
-import "./globals.css"
+import type React from "react";
+import type { Metadata } from "next";
+import "./globals.css";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Footyflix",
   description: "Watch football matches live and on demand.",
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-      </head>
-      <body>{children}</body>
-    </html>
-  )
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </Head>
+      {children}
+    </>
+  );
 }
